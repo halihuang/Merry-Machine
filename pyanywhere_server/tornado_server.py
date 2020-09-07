fbf50d3c6ad238032cfb5f7e043b311c2137924b
Amar Maksumić <amarmaksumich@gmail.com>
2020-09-07 01:19:29 -0400

    fixed pyanywhere tornado error

--- pyanywhere_server/tornado_server.py
+++ pyanywhere_server/tornado_server.py
@@ -1,7 +1,6 @@
 import tornado.web
 import tornado.wsgi
-from tornado.ioloop import IOLoop
-import config
+import requests
 import logging
 import json
 from model import predict_pos_labels, predict_neg_labels, predict_pol_labels, run_process
@@ -24,7 +23,7 @@ class NegModelHandler(tornado.web.RequestHandler):
     def post(self):
         try:
             encoded_sources = json.loads(self.request.body)
-            predictions = run_process(predict_pos_labels, encoded_sources)
+            predictions = run_process(predict_neg_labels, encoded_sources)
             if predictions is not None:
                 requests.post("https://merrymachine.herokuapp.com/predicter", json=predictions)
                 print('sent pos post')
@@ -55,7 +54,7 @@ class PolModelHandler(tornado.web.RequestHandler):
     def post(self):
         try:
             encoded_sources = json.loads(self.request.body)
-            predictions = run_process(predict_pos_labels, encoded_sources)
+            predictions = run_process(predict_pol_labels, encoded_sources)
             if predictions is not None:
                 requests.post("https://merrymachine.herokuapp.com/predicter", json=predictions)
                 print('sent pos post')
